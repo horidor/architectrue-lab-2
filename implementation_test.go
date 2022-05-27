e993bc2cf9da4e94bc95bb33dad0aa84ded985ff
gapiyka <50524296+gapiyka@users.noreply.github.com>
2022-05-27 21:04:13 +0300

    feat: add impementation tests

--- implementation_test.go
+++ implementation_test.go
@@ -1,23 +1,41 @@
 package lab2
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
+//	for assert equal expressions used online converter:
+//	https://www.free-online-calculator-use.com/prefix-to-postfix-converter.html
+
+func TestEasyPrefixToPostfix(t *testing.T) {
 	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
 	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
+		assert.Equal(t, "5 4 2 - 3 * +", res)
 	}
 }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
+func TestMoreDigitsPrefixToPostfix(t *testing.T) {
+	res, err := PrefixToPostfix("+ 52 * - 4 21 4")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "52 4 21 - 4 * +", res)
+	}
+}
 
-	// Output:
-	// 2 2 +
+func TestHardPrefixToPostfix(t *testing.T) {
+	res, err := PrefixToPostfix("- + - - 202 117 * 7 ^ 2 3 228 / 1337 191")
+	if assert.Nil(t, err) {
+		assert.Equal(t, "202 117 - 7 2 3 ^ * - 228 + 1337 191 / -", res)
+	}
+}
+
+func TestExceptionPrefixToPostfix(t *testing.T) {
+	_, err := PrefixToPostfix("- 2 * 2 A")
+	assert.NotNil(t, err)
+}
+
+func TestEmptyPrefixToPostfix(t *testing.T) {
+	_, err := PrefixToPostfix("")
+	assert.NotNil(t, err)
 }
